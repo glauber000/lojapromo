f71131fb597801ac54b68010a37999c03a9a0019
Glauber Oliveira <glauber17230@gmail.com>
2021-07-18 08:23:07 -0300

    alteracao

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    target: 'static',
     titleTemplate: '%s - gama_app',
     title: 'gama_app',
     htmlAttrs: {
