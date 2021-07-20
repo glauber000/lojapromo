6f00d1ec8f7afe45ba3373e661207023c2b1cd84
Glauber Oliveira <glauber17230@gmail.com>
2021-07-20 17:15:52 -0300

    Correção do tite

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   head: {
-    titleTemplate: '%s - gama_app',
+    titleTemplate: 'LOJAO DAS PROMOCOES',
     title: 'gama_app',
     htmlAttrs: {
       lang: 'en'
